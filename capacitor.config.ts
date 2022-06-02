308771fc9f5f6bb16123314fca00c44d8db3ad6b
puspendudas <puspendudas76@gmail.com>
2022-06-02 08:23:50 +0530

    First Update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'OfferLatter',
+  appId: 'com.puspendu.recipe',
+  appName: 'Recipe',
   webDir: 'www',
   bundledWebRuntime: false
 };
